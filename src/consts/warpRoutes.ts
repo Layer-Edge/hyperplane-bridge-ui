2483e8b9661b0bae46a3225c1a0b71251661e20d
vish-han <vshlchauhab@gmail.com>
2025-05-27 13:08:13 +0530

    correct configurations

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -1,38 +1,37 @@
 import { TokenStandard, WarpCoreConfig } from '@hyperlane-xyz/sdk';
 
-
 // A list of Warp Route token configs
 // These configs will be merged with the warp routes in the configured registry
 // The input here is typically the output of the Hyperlane CLI warp deploy command
 export const warpRouteConfigs: WarpCoreConfig = {
-  tokens: [
-    {
-      chainName: 'basesepolia',
-      standard: TokenStandard.EvmHypCollateral,
-      decimals: 18,
-      symbol: 'EDGEN',
-      name: 'LayerEdge',
-      addressOrDenom: '0x5ce55C6c132c5Aa04028635b8a83fB6940f392Da',
-      collateralAddressOrDenom: '0x64508Ee3af87a7F26D4F531B10E13585dA29f15b',
-      connections: [
+    tokens: [
         {
-          token: 'ethereum|bsctestnet|0x83c2084CC653c5149b3f429503188836fe280e71',
+            chainName: 'basesepolia',
+            standard: TokenStandard.EvmHypCollateral,
+            decimals: 18,
+            symbol: 'EDGEN',
+            name: 'LayerEdge',
+            addressOrDenom: '0x5ce55C6c132c5Aa04028635b8a83fB6940f392Da',
+            collateralAddressOrDenom: '0x64508Ee3af87a7F26D4F531B10E13585dA29f15b',
+            connections: [
+                {
+                    token: 'ethereum|bsctestnet|0x83c2084CC653c5149b3f429503188836fe280e71',
+                },
+            ],
         },
-      ],
-    },
-    {
-      chainName: 'bsctestnet',
-      standard: TokenStandard.EvmHypSynthetic,
-      decimals: 18,
-      symbol: 'EDGEN',
-      name: 'LayerEdge',
-      addressOrDenom: '0x83c2084CC653c5149b3f429503188836fe280e71',
-      connections: [
         {
-          token: 'ethereum|basesepolia|0x64508Ee3af87a7F26D4F531B10E13585dA29f15b', // Actual basesepolia token address
+            chainName: 'bsctestnet',
+            standard: TokenStandard.EvmHypSynthetic,
+            decimals: 18,
+            symbol: 'EDGEN',
+            name: 'LayerEdge',
+            addressOrDenom: '0x83c2084CC653c5149b3f429503188836fe280e71',
+            connections: [
+                {
+                    token: 'ethereum|basesepolia|0x5ce55C6c132c5Aa04028635b8a83fB6940f392Da', // Fixed: Use the actual token address
+                },
+            ],
         },
-      ],
-    },
-  ],
-  options: {},
+    ],
+    options: {},
 };
